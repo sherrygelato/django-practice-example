28e09a86d946d0d9f3c5ba0f4b1997c2fbf35436
sherrygelato <sherrygelato@kakao.com>
2021-11-23 09:46:35 +0900

    complete CRUD

--- articles/urls.py
+++ articles/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('', views.index),
     path('new/', views.new),
     path('create/', views.create),
-    path('<int:pk>/', views.detail),
+    path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete', views.delete),
     path('<int:pk>/edit', views.edit),
     path('<int:pk>/update', views.update),
+
 ]
